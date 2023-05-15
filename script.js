2d65f5ad4e6985c73d2f8fbb785fed93398c0718
Rik <ps205590@summacollege.nl>
2023-05-15 13:02:45 +0200

    added a clear button with it's functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let addTask = document.getElementById('addTask');
 let addBtn = document.getElementById('addBtn');
 let allTask = document.getElementById('allTask');
+let clearBtn = document.getElementById('clearBtn');
 
 addBtn.addEventListener('click', function(){
     var taskList = document.createElement('li');
@@ -13,4 +14,8 @@ addBtn.addEventListener('click', function(){
     taskList.addEventListener('dblclick', function(){
         allTask.removeChild(taskList);
     });
+})
+
+clearBtn.addEventListener('click', function() {
+    allTask.innerHTML = '';
 })
